7ef873d12461f93c4db488e3b84c4c9d933cc5e2
Nikolas Trivanovic <81877767+Teorija@users.noreply.github.com>
2021-10-07 14:24:27 -0400

    Update Task1.c

--- Task1.c
+++ Task1.c
@@ -28,16 +28,19 @@ void convertPotentiometerToDisplay(int ones, int tens) {
   //hardcoded array of digits 0-9 in BCD for the decoder inputs (A,B,C,D)
   //the first 4 elements of the array is the BCD (LSB- 1st, MSB - 4th)
   //the fifth element is the digit identifier for the potentiometer value passed in
-  int digits[10][5] {{0,0,0,0,0},
-     		             {1,0,0,0,1},
-     		             {0,1,0,0,2},
-       	             {1,1,0,0,3},
-        	           {0,0,1,0,4},
-         	           {1,0,1,0,5},
-          	         {0,1,1,0,6},
-           	         {1,1,1,0,7},
-            	       {0,0,0,1,8},
-             	       {1,0,0,1,9}}; 
+  int digits[10][5] 
+  {
+  {0,0,0,0,0},
+  {1,0,0,0,1},
+  {0,1,0,0,2},
+  {1,1,0,0,3},
+  {0,0,1,0,4},
+  {1,0,1,0,5},
+  {0,1,1,0,6},
+  {1,1,1,0,7},
+  {0,0,0,1,8},
+  {1,0,0,1,9}
+  }; 
   
   //given the ones and tens value run through the array of digits to find its BCD match
   //when a digit matches use the BCD associated with the digit as input for the decoder
